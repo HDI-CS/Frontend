5a38444ba5beee963a9043580cafb3a621f39700
Cuyqo <saon0410@gmail.com>
2026-04-10 22:47:01 +0900

    refactor: separate next config by env and fix s3 upload flow

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from 'next';
+const isDev = process.env.NODE_ENV === 'development';
+
 const ADMIN_APP_URL = process.env.ADMIN_APP_URL;
 
-console.log('ADMIN_APP_URL=', process.env.ADMIN_APP_URL);
-
-const nextConfig: NextConfig = {
+const commonConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -11,6 +11,19 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   transpilePackages: ['@hdi/ui', '@hdi/fonts'],
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'hdi-s3.s3.ap-northeast-2.amazonaws.com',
+        pathname: '/**',
+      },
+    ],
+    unoptimized: true,
+  },
+};
+const redirectConfig: NextConfig = {
   async redirects() {
     return [
       // Auth redirect to root
@@ -21,7 +34,20 @@ const nextConfig: NextConfig = {
       },
     ];
   },
-  // 개발 환경에서 API 프록시 설정 - 크로스 오리진 쿠키 문제 해결
+};
+
+const devConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8080/:path*',
+      },
+    ];
+  },
+};
+// 개발 환경에서 API 프록시 설정 - 크로스 오리진 쿠키 문제 해결
+const prodConfig: NextConfig = {
   async rewrites() {
     if (!ADMIN_APP_URL) {
       console.warn('⚠️ ADMIN_APP_URL is not set');
@@ -30,8 +56,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/:path*',
-        // destination: 'https://api.hdi.ai.kr/:path*',
-        destination: 'http://localhost:8080/:path*',
+        destination: 'https://api.hdi.ai.kr/:path*',
       },
       // admin API proxy
       {
@@ -48,12 +73,12 @@ const nextConfig: NextConfig = {
       },
     ];
   },
-  images: {
-    remotePatterns: [
-      new URL('https://hdi-s3.s3.ap-northeast-2.amazonaws.com/**'),
-    ],
-    unoptimized: true,
-  },
+};
+
+const nextConfig: NextConfig = {
+  ...commonConfig,
+  ...redirectConfig,
+  ...(isDev ? devConfig : prodConfig),
 };
 
 export default nextConfig;
